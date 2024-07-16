2aa01650477e1f3a6c30d519d8bb051a6114daa4
ValerioErnestoColibro <valerio.colibro@gmail.com>
2024-07-16 09:34:46 +0200

    Initialize boilerblade

--- src/models/device.ts
+++
@@ -1,9 +0,0 @@
-import { Auth } from "./auth";
-export class Device {
-  referenceKeyUser: Auth["referenceKeyUser"];
-  primarKeyDevice: number;
-  constructor(referenceKeyUser: Auth["referenceKeyUser"]) {
-    this.referenceKeyUser = referenceKeyUser;
-    this.primarKeyDevice = Math.random();
-  }
-}
